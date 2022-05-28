bc77c383b9a8b25d9713a91c8a5501a80c9392c5
Braydon Wang <braydon.wang@gmail.com>
2022-05-28 12:58:40 -0400

    Time: 1555 ms (5.07%), Space: 89.8 MB (82.91%) - LeetHub

--- 435-non-overlapping-intervals/435-non-overlapping-intervals.cpp
+++ 435-non-overlapping-intervals/435-non-overlapping-intervals.cpp
@@ -1,11 +1,14 @@
 bool comp(vector<int> &a, vector<int> &b) {
             return a[1] < b[1];
 }
-    
+
 class Solution {
 public:
     int eraseOverlapIntervals(vector<vector<int>>& intervals) {
         sort(intervals.begin(),intervals.end(),comp);
+        for (int i = 0; i < intervals.size(); i++) {
+            cout << intervals[i][0] << endl;
+        }
         int end = intervals[0][1];
         int ans = 1;
         for (int i = 1; i < intervals.size(); i++) {
