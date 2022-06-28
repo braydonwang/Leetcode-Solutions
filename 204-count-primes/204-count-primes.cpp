28fef326ff92e5550fa97cc1ed0530b5a6a004d2
Braydon Wang <braydon.wang@gmail.com>
2022-06-28 11:52:26 -0400

    Time: 336 ms (44.82%), Space: 10.3 MB (33.77%) - LeetHub

--- 204-count-primes/204-count-primes.cpp
+++ 204-count-primes/204-count-primes.cpp
@@ -3,14 +3,18 @@ public:
     int countPrimes(int n) {
         vector<bool> prime(n,true);
         int ans = 0;
-        for (int i = 2; i < n; i++) {
+        for (int i = 2; i*i < n; i++) {
             if (prime[i]) {
-                ans++;
-                int temp = i+i;
+                int temp = i*i;
                 while (temp < n) {
                     prime[temp] = false;
                     temp += i;
                 }
+            }
+        }
+        for (int i = 2; i < n; i++) {
+            if (prime[i]) {
+                ans++;
             }
         }
         return ans;
