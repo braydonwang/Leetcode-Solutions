c581ddfa1e422af7e900545a2b1f940f3d722aa0
Braydon Wang <braydon.wang@gmail.com>
2022-06-29 14:39:07 -0400

    Time: 110 ms (38.28%), Space: 16.5 MB (88.10%) - LeetHub

--- 221-maximal-square/221-maximal-square.cpp
+++ 221-maximal-square/221-maximal-square.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     int maximalSquare(vector<vector<char>>& matrix) {
-        vector<int> pre(matrix[0].size());
         vector<int> dp(matrix[0].size());
-        int ans = 0;
+        int ans = 0, pre = 0;
         for (int i = 0; i < matrix.size(); i++) {
             for (int j = 0; j < matrix[i].size(); j++) {
+                int temp = dp[j];
                 if (i == 0 || j == 0 || matrix[i][j] == '0') {
                     dp[j] = matrix[i][j] - '0';
                 } else if (matrix[i][j] == '1') {
-                    dp[j] = min(dp[j-1],min(pre[j],pre[j-1])) + 1;
+                    dp[j] = min(pre,min(dp[j],dp[j-1])) + 1;
                 }
                 ans = max(ans,dp[j]);
+                pre = temp;
             }
-            pre = dp;
         }
         return ans*ans;
     }
