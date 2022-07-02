a804f6dc8eca2c7edbc21008188174ac69142f41
Braydon Wang <braydon.wang@gmail.com>
2022-07-02 13:06:16 -0400

    Time: 9 ms (60.26%), Space: 5.9 MB (74.81%) - LeetHub

--- 223-rectangle-area/223-rectangle-area.cpp
+++ 223-rectangle-area/223-rectangle-area.cpp
@@ -1,15 +1,6 @@
 class Solution {
 public:
     int computeArea(int a, int b, int c, int d, int e, int f, int g, int h) {
-        int left = max(a,e);
-        int right = min(c,g);
-        int top = min(d,h);
-        int bot = max(b,f);
-        
-        int overlap = 0;
-        if (right > left && top > bot) {
-            overlap = (top - bot) * (right - left);
-        }
-        return (c - a) * (d - b) + (g - e) * (h - f) - overlap;
+        return (c - a) * (d - b) + (g - e) * (h - f) - max((min(d,h) - max(b,f)),0) * max((min(c,g) - max(a,e)),0);
     }
 };
